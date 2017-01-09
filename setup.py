6af7e4654ef2e4dd2c0a8d39a2816fe7a9ab4b12
David Sanders <davesque@gmail.com>
2017-01-09 14:00:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='0.1',
     description='iTerm color scheme conversion tools',
     keywords='iterm convert color scheme',
-    url='http://github.com/davesque/iterm-convert',
+    url='http://github.com/davesque/iterm_convert',
     author='David Sanders',
     author_email='davesque@gmail.com',
     license='MIT',
